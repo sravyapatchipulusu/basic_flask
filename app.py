db853366a38d97eb4df56e95ce29bc91a32c1e1c
sravyapatchipulusu <sravyap986@gmail.com>
2025-03-11 23:59:21 +0530

    new change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Snyk Security Scan!"
+    return "Hello, nice to meet you snyk"
 
 if __name__ == "__main__":
     app.run(debug=True)
